e4f41287e9104c65a86ab8db3d69d2b0eca9f11a
Kisly93 <spsvk.kis.sov@gmail.com>
2023-11-30 19:25:32 +0100

    fixed code

--- bot_tg.py
+++ bot_tg.py
@@ -11,16 +11,24 @@ bot = telebot.TeleBot(os.getenv('TELEGRAM_BOT_API'))
 redis_db = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'),
                              password=os.getenv('REDIS_PASSWORD'), db=0)
 
+user_data = {}
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Привет! Я - бот викторины.")
+    chat_id = message.chat.id
+
+    if 'questions_and_answers_from_file' not in user_data.get(chat_id, {}):
+        questions_and_answers_from_file = load_questions_answers()
+        user_data[chat_id] = {'questions_and_answers_from_file': questions_and_answers_from_file}
+
+    bot.send_message(chat_id, "Привет! Я - бот викторины.")
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = types.KeyboardButton('Новый вопрос')
     button2 = types.KeyboardButton('Сдаться')
     button3 = types.KeyboardButton('Мой счет')
     markup.add(button1, button2, button3)
-    bot.send_message(message.chat.id, "Нажмите новый вопрос для начала викторины!\n /cancel - для выхода",
+    bot.send_message(chat_id, "Нажмите новый вопрос для начала викторины!\n /cancel - для выхода",
                      reply_markup=markup)
 
 
@@ -33,12 +41,13 @@ def command_default(message):
 def handle_telegram_messages(message):
     chat_id = message.chat.id
     user_answer = message.text
-    questions_and_answers = load_questions_answers()
+    questions_and_answers = user_data.get(chat_id, {}).get('questions_and_answers_from_file', {})
 
     if user_answer == 'Новый вопрос':
         random_question = get_random_question(questions_and_answers)
         redis_db.set(chat_id, random_question)
         bot.send_message(chat_id, f"Вопрос: {random_question}")
+
     else:
         correct_answer = questions_and_answers.get(redis_db.get(chat_id).decode('utf-8'))
         if user_answer.lower() == 'сдаться':
